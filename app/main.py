a6584e2fe6a4d8afe607634daeac7966f76bba97
Rasul <RasulGaleev2005@yandex.ru>
2024-02-24 13:22:49 +0300

    Переписал CORS

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api import router as api_router
+
+origins = ["http://92.38.48.73", "http://localhost:8000"]
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=False,
+        allow_methods=["GET", "POST", "DELETE"],
+        allow_headers=["Content-Type", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"]
+    )
+]
 
 app = FastAPI(
     title="Muslim Web Bot",
     docs_url="/api/docs",
     redoc_url="/api/redoc",
     openapi_url=f"/api/openapi.json",
+    middleware=middleware,
 )
+app.include_router(api_router)
 
 # template_dir = os.path.join("app", "templates")
 # templates = Jinja2Templates(directory=template_dir)
@@ -19,7 +34,6 @@ app = FastAPI(
 # app.mount("/css", StaticFiles(directory=os.path.join(static_dir, "css")), name="css")
 # app.mount("/media", StaticFiles(directory=os.path.join(static_dir, "media")), name="media")
 
-app.include_router(api_router)
 
 # @app.middleware("http")
 # async def add_cors_headers(request, call_next):
@@ -39,15 +53,14 @@ app.include_router(api_router)
 # async def dua_page(request: Request, item_id: int = Query(..., alias="itemId")):
 #     return templates.TemplateResponse("dua.html", {"request": request, "item_id": item_id})
 
-origins = ["http://92.38.48.73", "http://localhost:8000"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=False,
-    allow_methods=["GET", "POST", "DELETE"],
-    allow_headers=["Content-Type", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=False,
+#     allow_methods=["GET", "POST", "DELETE"],
+#     allow_headers=["Content-Type", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"],
+# )
 
 # if __name__ == '__main__':
 #     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
